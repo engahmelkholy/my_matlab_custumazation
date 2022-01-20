bfb573bc139d1c64dc5f0cd2ca89d194f4bb4c28
Ahmed_Elkholy <ahmed_elkholy@f-eng.tanta.edu.eg>
2022-01-20 16:39:02 +0300

    adding help to go function

--- go.m
+++ go.m
@@ -1,11 +1,18 @@
 function go(n)
+    %% go o,q >> open, quit the matlab
+    %% go home,work,tst >> go to fixed folder ahk, matlab work dir, go test folder
+    %% go to for current project it will change respectifilly
+    %% "p1,wave,wavee,p7,p52,genlag"
+    %% go d, dd, ddd / go c, cc, ccc >> vairuis clearing options and cd options
 switch n
     case 'o'
     winopen(pwd)
     case 'work'
         cd 'C:\Users\ahm_e\Documents\MATLAB'
     case 'home'
-        cd 'D:\OneDrive - Fculty Of Engineering (Tanta University)\General_software\scripts\AHK'
+    cd 'D:\OneDrive - Fculty Of Engineering (Tanta University)\General_software\scripts\AHK'
+        case 'tst'
+    cd 'C:\Users\ahm_e\Documents\MATLAB\Test'
     case 'p1'
        cd 'C:\Users\ahm_e\Documents\MATLAB\micro_grid\phase_locked_loop\project PLL'     
     %case 'zth'
@@ -20,8 +27,6 @@ switch n
         addpath(p);
         clear p
        cd 'C:\Users\ahm_e\Documents\MATLAB\WEC-Sim_Applications\Elec_wave'
-    case 'tst'
-    cd 'C:\Users\ahm_e\Documents\MATLAB\Test'
     case 'p7'
     cd 'C:\Users\ahm_e\Documents\MATLAB\SVC-AUTO'
     case 'p52'
